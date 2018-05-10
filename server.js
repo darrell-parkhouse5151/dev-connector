8d034c75f88c490fb8d0923c2373cf72e4c19401
darrell-parkhouse5151 <lazycoder5151@gmail.com>
2018-05-10 09:03:54 -0500

    initial commit

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
